3b5f690a742f269d71270408cd4d5f499b0391eb
Nathaniel Bomberger <nathaniel.bomberger@gmail.com>
2020-08-13 03:32:59 -0400

    Removed object files from Marlin source code

--- Marlin/.pio/build/STM32F103RC_btt_512K/lib57e/USBComposite/usb_scsi_data.c.d
+++
@@ -1,5 +0,0 @@
-.pio/build/STM32F103RC_btt_512K/lib57e/USBComposite/usb_scsi_data.c.o: \
- /Users/nbomberger/.platformio/packages/framework-arduinoststm32-maple/STM32F1/libraries/USBComposite/usb_scsi_data.c \
- /Users/nbomberger/.platformio/packages/framework-arduinoststm32-maple/STM32F1/libraries/USBComposite/usb_scsi.h
-
-/Users/nbomberger/.platformio/packages/framework-arduinoststm32-maple/STM32F1/libraries/USBComposite/usb_scsi.h:
